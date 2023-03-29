5a2210336c49bd82b032c9ae4dca4a4764a46577
alejandroTTT <lejandro@twototango.co>
2023-03-29 02:55:40 +0000

    Fix: Adding some fixes in action :cat:

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         ) : (
           <>
             <img src={imageRickMorty} alt="Rick & Morty" className="img-home"></img>
-            <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+            <button onClick={reqApi} className="btn-search">Buscar Personajes :</button>
           </>
         ) }
       </header>
